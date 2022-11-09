e270389f1487c1bf56382e59bafbb5333d03262f
niharika <nsingh@diligent.com>
2022-11-09 16:12:23 +0530

    file_system_cmd: tasks app assignment

--- notes-app/app.js
+++ notes-app/app.js
@@ -3,8 +3,58 @@
 // fs.appendFileSync('notes.txt', 'whatsup');
 const validator = require('validator')
 const chalk = require('chalk')
-
 const notes = require('./notes')
-console.log(chalk.green.bgBlue.inverse.bold(notes() ))
-console.log(validator.isEmail('abc@xyz.com') )
-console.log(process.argv[2])
+
+
+const yargs = require('yargs')
+const { demandOption } = require('yargs')
+yargs.version('1.1.0')
+
+
+//create add command
+yargs.command({
+    command: 'add',
+    describe: 'add a note',
+    builder: {
+        title:{
+            describe: 'add a title',
+            demandOption:true,
+            type:'string'
+        },
+        body:{
+            describe:'note',
+            demandOption:true
+        }
+    },
+
+    handler(argv) {
+        //console.log('adding a note', argv.title)
+        notes.add( argv.title, argv.body)
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'remove a note',
+    builder:{
+        title:{
+            describe:'remove',
+            demandOption: true
+        } 
+
+    },
+    handler(argv) {
+        console.log('removing a note')
+        notes.remove(argv.title)
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'read a note',
+    handler: function () {
+        console.log('reading a note')
+    }
+})
+
+console.log(yargs.argv)
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,6 +1,55 @@
-console.log("hi")
-const fun = function(){
-    return "here are your notes";
+//console.log("hi")
+const fs=require('fs')
+
+const savenotes= (notes)=> {
+    const data = JSON.stringify(notes)
+    fs.writeFileSync('data.json', data)
 }
 
-module.exports = fun
+const load = () => {
+    try{
+        buffer = fs.readFileSync('data.json')
+        string_data = buffer.toString()
+        console.log(string_data)
+        parsed = JSON.parse(string_data)
+        console.log(parsed)
+        return parsed
+    }catch{
+        return []
+    }
+
+
+}
+const add = (title, body) => {
+    const notes = load()
+    
+    const dups = notes.filter( (note) =>  note.title===title   )
+
+    if (dups.length === 0){
+        notes.push({
+            'title':title,
+            'body':body
+        })
+
+        savenotes(notes)
+    }else{
+        console.log('noted!!!!')
+    }
+
+
+}
+
+const remove = (title) => {
+    const notes = load()
+
+    const notes_to_keep = notes.filter( (note)=> note.title !==title )
+
+    savenotes(notes_to_keep)
+}
+
+module.exports={
+    'add':add,
+    'remove': remove
+}
+
+    
