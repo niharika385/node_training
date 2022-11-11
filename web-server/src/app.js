91fce075353d6d2be02dd4f741382851b842d3d1
niharika <nsingh@diligent.com>
2022-11-11 11:45:17 +0530

    weather_app:part-2

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,19 +1,29 @@
 const express = require('express')
+const hbs = require('hbs')
 
 const path = require('path')
 console.log(__dirname)
 const pub_dir = path.join(__dirname, '../public')
 const templates = path.join(__dirname, '../templates')
+const partials = path.join(__dirname, '../templates/partials')
+
+
+
 console.log(pub_dir)
 app = express()
 
 app.set('view engine', 'hbs')
 app.use(express.static(pub_dir) )
+hbs.registerPartials(partials)
 app.set('views', templates)
 
 
 app.get('', (req, res) => {
-    res.render('index', {name:'niharika', lastname: 'singh'})
+   // res.render('index', {name:'niharika', lastname: 'singh'})
+   res.send({
+    data: req.query.add
+   })
+    
 })
 
 // app.get('/help', (req, res) => {
